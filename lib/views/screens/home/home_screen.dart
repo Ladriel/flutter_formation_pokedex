9c3a0043b34e81095b4acb243d26999931b4d7b4
Anthony BORNER <Anthony.BORNER.ext@boursorama.fr>
2025-01-31 15:55:25 +0100

    feat: new header bar widget

--- lib/views/screens/home/home_screen.dart
+++ lib/views/screens/home/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_formation_pokedex/core/strings.dart';
 import 'package:flutter_formation_pokedex/views/screens/pokedex/pokedex_screen.dart';
+import 'package:flutter_formation_pokedex/views/widgets/header_bar.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key, required this.title});
@@ -14,9 +16,8 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+      appBar: HeaderBar(
+        title: Strings.homeTitle,
       ),
       body: PokedexScreen(),
     );
