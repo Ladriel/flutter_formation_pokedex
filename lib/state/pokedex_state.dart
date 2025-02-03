c8892da69ee2e574892b56682601efca864f4ff8
Anthony BORNER <Anthony.BORNER.ext@boursorama.fr>
2025-02-03 13:30:21 +0100

    feat: move init in state object

--- lib/state/pokedex_state.dart
+++ lib/state/pokedex_state.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_formation_pokedex/data/models/pokedex_entry.dart';
+import 'package:flutter_formation_pokedex/data/poke_api.dart';
+import 'package:flutter_formation_pokedex/data/repository.dart';
 
 class PokedexState with ChangeNotifier {
   bool loading = true;
   String? error;
   List<PokedexEntry> entries = <PokedexEntry>[];
 
-  PokedexState();
+  PokedexState() {
+    fetchPokedexEntries();
+  }
 
-  fetch() {
-    //TODO
+  fetchPokedexEntries() async {
+    // loading true by default
+    Repository repository = Repository(pokemonAPIClient: PokeAPIClient());
+    try {
+      entries = await repository.getPokedexEntries();
+    } catch (e) {
+      print("error $e");
+      error = e.toString();
+    }
+    loading = false;
+    notifyListeners();
   }
 }
