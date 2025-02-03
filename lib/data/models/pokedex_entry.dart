161a4ee55f7f3ba2dd5cd3fc999f286adbc4ed7c
Anthony BORNER <Anthony.BORNER.ext@boursorama.fr>
2025-02-03 16:58:31 +0100

    fix: model should correspond in entry and output

--- lib/data/models/pokedex_entry.dart
+++ lib/data/models/pokedex_entry.dart
@@ -1,12 +1,18 @@
 class PokedexEntry {
   String name;
   int number;
+  String url;
   String sprite;
+
+  static String buildSpriteUrl(int number) {
+    return "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$number.png";
+  }
 
 //<editor-fold desc="Data Methods">
   PokedexEntry({
     required this.name,
     required this.number,
+    required this.url,
     required this.sprite,
   });
 
@@ -17,16 +23,19 @@ class PokedexEntry {
           runtimeType == other.runtimeType &&
           name == other.name &&
           number == other.number &&
+          url == other.url &&
           sprite == other.sprite);
 
   @override
-  int get hashCode => name.hashCode ^ number.hashCode ^ sprite.hashCode;
+  int get hashCode =>
+      name.hashCode ^ number.hashCode ^ url.hashCode ^ sprite.hashCode;
 
   @override
   String toString() {
     return 'PokedexEntry{' +
         ' name: $name,' +
         ' number: $number,' +
+        ' url: $url,' +
         ' sprite: $sprite,' +
         '}';
   }
@@ -34,11 +43,13 @@ class PokedexEntry {
   PokedexEntry copyWith({
     String? name,
     int? number,
+    String? url,
     String? sprite,
   }) {
     return PokedexEntry(
       name: name ?? this.name,
       number: number ?? this.number,
+      url: url ?? this.url,
       sprite: sprite ?? this.sprite,
     );
   }
@@ -47,6 +58,7 @@ class PokedexEntry {
     return {
       'name': this.name,
       'number': this.number,
+      'url': this.url,
       'sprite': this.sprite,
     };
   }
@@ -61,12 +73,9 @@ class PokedexEntry {
     return PokedexEntry(
       name: map['name'] as String,
       number: number,
+      url: url,
       sprite: spriteUrl,
     );
-  }
-
-  static String buildSpriteUrl(int number) {
-    return "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$number.png";
   }
 
 //</editor-fold>
