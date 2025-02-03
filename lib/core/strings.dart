91b246254dc87e807c33a8a1515ee66aab9eaabe
Anthony BORNER <Anthony.BORNER.ext@boursorama.fr>
2025-02-03 10:02:17 +0100

    feat: adding favorites screen

--- lib/core/strings.dart
+++ lib/core/strings.dart
@@ -2,4 +2,5 @@ class Strings {
   static const appTitle = "Flutter formation Pokedex";
   static const homeTitle = "Home";
   static const detailScreenTitle = "Détail";
+  static const favoritesScreenTitle = "Favs";
 }
---
+++ lib/views/screens/favorites/favorites_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_formation_pokedex/core/strings.dart';
+import 'package:flutter_formation_pokedex/views/widgets/header_bar.dart';
+
+class FavoritesScreen extends StatelessWidget {
+  const FavoritesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      appBar: HeaderBar(
+        title: Strings.favoritesScreenTitle,
+      ),
+      body: Center(
+        child: Text(Strings.favoritesScreenTitle),
+      ),
+    );
+  }
+}
