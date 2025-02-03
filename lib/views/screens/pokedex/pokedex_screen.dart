589d9eeba4ddbd6364b6b34217cbe533b3869176
Anthony BORNER <Anthony.BORNER.ext@boursorama.fr>
2025-02-03 11:13:23 +0100

    feat: switch pokedexScreen to statefull

--- lib/views/screens/pokedex/pokedex_screen.dart
+++ lib/views/screens/pokedex/pokedex_screen.dart
@@ -2,14 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:flutter_formation_pokedex/data/models/pokedex_entry.dart';
 import 'package:flutter_formation_pokedex/views/screens/pokedex/components/pokedex_entry_card.dart';
 
-class PokedexScreen extends StatelessWidget {
-  PokedexScreen({super.key});
+class PokedexScreen extends StatefulWidget {
+  const PokedexScreen({super.key});
 
-  final List<PokedexEntry> entries = <PokedexEntry>[
-    PokedexEntry(name: "Bulbizarre", number: 1, sprite: "1.png"),
-    PokedexEntry(name: "Salamèche", number: 4, sprite: "4.png"),
-    PokedexEntry(name: "Carapuce", number: 7, sprite: "7.png"),
-  ];
+  @override
+  State<PokedexScreen> createState() => _PokedexScreenState();
+}
+
+class _PokedexScreenState extends State<PokedexScreen> {
+  bool loading = true;
+  String? error;
+  List<PokedexEntry> entries = <PokedexEntry>[];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
