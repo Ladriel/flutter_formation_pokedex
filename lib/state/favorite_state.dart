99e6491f1081ae5be056095a437ba65adc912d7d
Anthony BORNER <Anthony.BORNER.ext@boursorama.fr>
2025-02-03 16:58:14 +0100

    feat: hydrate fav state model from prefs

--- lib/state/favorite_state.dart
+++ lib/state/favorite_state.dart
@@ -1,20 +1,41 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_formation_pokedex/data/models/pokedex_entry.dart';
-//import 'package:provider/provider.dart';
+import 'package:flutter_formation_pokedex/data/repository.dart';
 
 class FavoritesState with ChangeNotifier {
+  FavoritesState({List<PokedexEntry>? favs}) {
+    if (favs != null) {
+      favorites = favs;
+    } else {
+      initFavorites();
+    }
+  }
+
   List<PokedexEntry> _favorites = [];
 
   List<PokedexEntry> get favorites => _favorites;
 
   set favorites(List<PokedexEntry> value) {
     _favorites = value;
+    Repository().setStorageValue(Repository.favoriteStorageKey, toJson());
     notifyListeners();
+  }
+
+  Future<void> initFavorites() async {
+    String? storageFavs =
+        await Repository().getStorageValue(Repository.favoriteStorageKey);
+    if (storageFavs != null) {
+      FavoritesState state = FavoritesState.fromJson(storageFavs);
+      _favorites = state.favorites;
+      notifyListeners();
+    }
   }
 
   addFavorite(PokedexEntry value) {
     if (!isFavorite(value)) {
       _favorites.add(value);
+      Repository().setStorageValue(Repository.favoriteStorageKey, toJson());
       notifyListeners();
     }
   }
@@ -22,6 +43,7 @@ class FavoritesState with ChangeNotifier {
   removeFavorite(PokedexEntry value) {
     if (isFavorite(value)) {
       _favorites.remove(value);
+      Repository().setStorageValue(Repository.favoriteStorageKey, toJson());
       notifyListeners();
     }
   }
@@ -37,4 +59,25 @@ class FavoritesState with ChangeNotifier {
       addFavorite(value);
     }
   }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'favorites': favorites.map((x) => x.toMap()).toList(),
+    };
+  }
+
+  factory FavoritesState.fromMap(Map<String, dynamic> map) {
+    return FavoritesState(
+      favs: List<PokedexEntry>.from(
+        (map['favorites'] as List<dynamic>).map<PokedexEntry>(
+          (x) => PokedexEntry.fromMap(x as Map<String, dynamic>),
+        ),
+      ),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory FavoritesState.fromJson(String source) =>
+      FavoritesState.fromMap(json.decode(source) as Map<String, dynamic>);
 }
