215c27a8d11ed4918e0f093dcedfd9bc230d958e
Anthony BORNER <Anthony.BORNER.ext@boursorama.fr>
2025-02-04 11:06:09 +0100

    feat: entryCard stack

--- lib/views/screens/pokedex/components/pokedex_entry_card.dart
+++ lib/views/screens/pokedex/components/pokedex_entry_card.dart
@@ -34,8 +34,60 @@ class PokedexEntryCard extends StatelessWidget {
           );
         },
         child: Card(
-          child: Text(entry.name),
+          child: _buildCard(context, entry),
         ),
+      ),
+    );
+  }
+
+  _buildCard(BuildContext context, PokedexEntry entry) {
+    ThemeData theme = Theme.of(context);
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Stack(
+        children: [
+          Positioned(
+            top: 1,
+            left: 1,
+            child: CircleAvatar(
+              maxRadius: 20,
+              child: Text(entry.number.toString()),
+            ),
+          ),
+          Positioned.fill(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.network(
+                entry.sprite,
+                fit: BoxFit.contain,
+                frameBuilder: (BuildContext context, Widget child, int? frame,
+                    bool wasSynchronouslyLoaded) {
+                  return frame != null
+                      ? child
+                      : Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Image.asset(
+                            'assets/pokeball.png',
+                            fit: BoxFit.contain,
+                          ),
+                        );
+                },
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 1,
+            right: 1,
+            child: Text(
+              "${entry.name[0].toUpperCase()}${entry.name.substring(1)}",
+              style: TextStyle(
+                fontSize: 15,
+                color: theme.colorScheme.onSurface,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
